89d559dee673453d68a1a1df565faac8a48b7d43
Eric Vo <vo.eric@gmail.com>
2017-12-06 15:39:40 -0500

    add search of videos on render

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <h3>Here is the search bar</h3>
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,26 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCKNSAM4DdCJtkhB-Fhqy3x6MHRAcY3caw';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({key: API_KEY, term: 'myon and shane 54'}, (videos) => {
+      this.setState({ videos });
+      console.log(videos);
+    })
+  }
+
   render() {
     return (
       <div>
-        <h1>This is VideoFetcher</h1>
+        <SearchBar />
       </div>
     );
   }
