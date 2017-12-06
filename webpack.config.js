dc11a7d0ae21ee4f638d94e662c7d099f46e361b
Eric Vo <vo.eric@gmail.com>
2017-12-06 15:07:56 -0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: ["./src/index.jsx"],
+  output: {
+    path: __dirname,
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
