e9dd3f940e92d24b2c61303a60be252ba64f9b06
Eric Vo <vo.eric@gmail.com>
2020-05-12 21:26:44 -0700

    change api key to one that works

--- src/index.jsx
+++ src/index.jsx
@@ -7,38 +7,42 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyCKNSAM4DdCJtkhB-Fhqy3x6MHRAcY3caw';
-
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      videos: [] ,
-      selectedVideo: null
+      videos: [],
+      selectedVideo: null,
     };
   }
 
   videoSearch(term) {
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-      this.setState({videos});
-      if (!this.state.selectedVideo) {
-        this.setState({selectedVideo: videos[0]});
+    YTSearch(
+      { key: 'AIzaSyBhlx6f-XRjrT0MWLxT2vfaxrNSOIKIPnY', term: term },
+      videos => {
+        this.setState({ videos });
+        if (!this.state.selectedVideo) {
+          this.setState({ selectedVideo: videos[0] });
+        }
       }
-    });
+    );
   }
 
   render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
 
     return (
-      <div className="main-page">
+      <div className='main-page'>
         <SearchBar onSearchTermChange={videoSearch} />
-        <div className="video-main">
+        <div className='video-main'>
           <VideoDetail video={this.state.selectedVideo} />
           <VideoList
-            onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-            videos={this.state.videos} />
+            onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+            videos={this.state.videos}
+          />
         </div>
       </div>
     );
